078249f4de58ae1b442bb9677e403b760a9a7617
Joe Amenta <airbreather@linux.com>
2009-05-29 15:16:20 -0400

    Preliminary str->unicode fix.

---
+++ lib3to2/fixes/fix_str.py
@@ -0,0 +1,27 @@
+"""Fixer that changes str to unicode, chr to unichr, and "..." into u"...".
+
+"""
+
+import re
+from lib2to3.pgen2 import token
+from lib2to3 import fixer_base
+
+class FixUnicode(fixer_base.BaseFix):
+
+    PATTERN = "STRING | NAME<'str' | 'chr'>"
+
+    def transform(self, node, results):
+        if node.type == token.NAME:
+            if node.value == u"str":
+                new = node.clone()
+                new.value = u"unicode"
+                return new
+            if node.value == u"chr":
+                new = node.clone()
+                new.value = u"unichr"
+                return new
+        elif node.type == token.STRING:
+            if re.match(ur"[rR]?[\'\"]", node.value):
+                new = node.clone()
+                new.value = u"u" + new.value
+                return new
