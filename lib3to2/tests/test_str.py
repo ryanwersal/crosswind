f02cec88f78d695463dedc5c8379d3a99eb65eab
Joe Amenta <airbreather@linux.com>
2009-05-29 17:30:33 -0400

    Added chr call to test_str.py, also changing names of tests.

--- lib3to2/tests/test_str.py
+++ lib3to2/tests/test_str.py
@@ -3,22 +3,27 @@ from lib3to2.tests.test_all_fixers import lib3to2FixerTestCase
 class Test_str(lib3to2FixerTestCase):
     fixer = "str"
 
-    def test_unicode_call(self):
+    def test_str_call(self):
         b = """str(x, y, z)"""
         a = """unicode(x, y, z)"""
         self.check(b, a)
+    
+    def test_chr_call(self):
+        b = """chr(a, t, m)"""
+        a = """unichr(a, t, m)"""
+        self.check(b, a)
 
-    def test_unicode_literal_1(self):
+    def test_str_literal_1(self):
         b = '''"x"'''
         a = '''u"x"'''
         self.check(b, a)
 
-    def test_unicode_literal_2(self):
+    def test_str_literal_2(self):
         b = """r'x'"""
         a = """ur'x'"""
         self.check(b, a)
 
-    def test_unicode_literal_3(self):
+    def test_str_literal_3(self):
         b = """R'''x'''"""
         a = """uR'''x'''"""
         self.check(b, a)
