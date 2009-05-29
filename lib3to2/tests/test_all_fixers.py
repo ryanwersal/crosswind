4ca2a235989f0b83b5e0d1fe6182927587006985
Joe Amenta <airbreather@linux.com>
2009-05-29 14:45:11 -0400

    Simplifying lib3to2FixerTestCase.setUp()

--- lib3to2/tests/test_all_fixers.py
+++ lib3to2/tests/test_all_fixers.py
@@ -19,16 +19,7 @@ from lib2to3.tests.test_fixers import FixerTestCase
 
 class lib3to2FixerTestCase(FixerTestCase):
     def setUp(self, fix_list=None, fixer_pkg="lib3to2"):
-        if fix_list is None:
-            fix_list = [self.fixer]
-        options = {"print_function" : False}
-        self.refactor = support.get_refactorer(fixer_pkg=fixer_pkg, fixers=fix_list, options=options)
-        self.fixer_log = []
-        self.filename = u"<string>"
-
-        for fixer in chain(self.refactor.pre_order,
-                           self.refactor.post_order):
-            fixer.log = self.fixer_log
+        super(lib3to2FixerTestCase, self).setUp(fixer_pkg=fixer_pkg)
 
 if __name__ == "__main__":
     for module in os.listdir(os.path.split(__file__)[0]):
