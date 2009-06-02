5e5017d2c605abac55116bf944e14f3e6d4d4cc5
Joe Amenta <airbreather@linux.com>
2009-06-02 16:06:56 -0400

    memoryview(...) -> buffer(...), requires explicit invocation

---
+++ lib3to2/fixes/fix_memoryview.py
@@ -0,0 +1,22 @@
+"""
+Fixer that changes memoryview(...) into buffer(...)
+Many memoryview(...) methods are invalid on buffer(...) objects,
+so the user must explicitly convert between them.
+"""
+
+from lib2to3 import fixer_base
+from lib2to3.fixer_util import Name
+
+
+class FixMemoryview(fixer_base.BaseFix): 
+
+    explicit = True # User must specify that they want this.
+
+    PATTERN = """
+              power< name='memoryview' trailer< '(' [any] ')' >
+              rest=any* >
+              """
+
+    def transform(self, node, results):
+        name = results["name"]
+        name.replace(Name(u"buffer", prefix=name.get_prefix()))
