93e3547fc696303916b438804f346bd996878f24
Joe Amenta <airbreather@linux.com>
2009-06-04 17:04:45 -0400

    Ugly, but next works now.

---
+++ lib3to2/fixes/fix_next.py
@@ -0,0 +1,40 @@
+"""Fixer for it.__next__() or next(it) -> it.next()"""
+
+from copy import deepcopy
+
+from lib2to3.pgen2 import token
+from lib2to3.pygram import python_symbols as syms
+from lib2to3 import fixer_base
+from lib2to3.fixer_util import Name, Call, find_binding, Attr
+
+bind_warning = "Calls to builtin next() possibly shadowed by global binding"
+
+
+class FixNext(fixer_base.BaseFix):
+    PATTERN = """
+    power< base=any+ trailer< '.' attr='__next__' > any* >
+    |
+    power< head='next' trailer< '(' arg=any ')' > any* >
+    |
+    classdef< 'class' base=any+ ':'
+              suite< any*
+                     funcdef< 'def'
+                              attr='__next__'
+                              parameters< '(' NAME ')' > any+ >
+                     any* > >
+    """
+
+    def transform(self, node, results):
+        assert results
+
+        base = results.get("base")
+        attr = results.get("attr")
+        head = results.get("head")
+        arg_ = results.get("arg")
+        arg = deepcopy(arg_)
+        if arg:
+            head.replace(Attr(Name(unicode(arg),prefix=head.prefix),
+                              Name(u"next")))
+            arg_.remove()
+        elif base:
+            attr.replace(Name(u"next", prefix=attr.prefix))
