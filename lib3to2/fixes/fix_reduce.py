ee2f82faaab65e8d533f469c830bb2c618650fbd
Joe Amenta <airbreather@linux.com>
2009-07-12 14:53:22 -0400

    Added reduce with tests

---
+++ lib3to2/fixes/fix_reduce.py
@@ -0,0 +1,36 @@
+"""
+Fixer for:
+functools.reduce(f, it) -> reduce(f, it)
+from functools import reduce -> (remove this line)
+"""
+
+from lib2to3 import fixer_base
+from lib2to3.fixer_util import Call
+from lib2to3.pytree import Node, Leaf
+from lib2to3.pgen2 import token
+
+def orphan_all(parent):
+    for child in parent.children:
+        if type(child) == Node:
+            orphan_all(child)
+        else:
+            child.remove()
+
+class FixReduce(fixer_base.BaseFix):
+    
+    PATTERN = """
+    power< 'functools' trailer< '.' 'reduce' > 
+                                    args=trailer< '(' arglist< any* > ')' > > |
+    imported=import_from< 'from' 'functools' 'import' 'reduce' >
+    """
+    
+    def transform(self, node, results):
+        syms = self.syms
+        args, imported = (results.get("args"), results.get("imported"))
+        if imported:
+            imported.remove()
+        elif args:
+            args = args.clone()
+            prefix = node.prefix
+            return Node(syms.power, [Leaf(token.NAME, u"reduce"), args],
+                                                                 prefix=prefix)
---
+++ lib3to2/tests/test_reduce.py
@@ -0,0 +1,34 @@
+from test_all_fixers import lib3to2FixerTestCase
+
+class Test_reduce(lib3to2FixerTestCase):
+    fixer = "reduce"
+    
+    def test_functools_import(self):
+        b = """
+            from functools import reduce
+            reduce(f, it)
+            """
+        a = """
+            reduce(f, it)
+            """
+        self.check(b, a)
+    
+    def test_functools_reduce(self):
+        b = """
+            import functools
+            functools.reduce(spam, ['spam', 'spam', 'baked beans', 'spam'])
+            """
+        a = """
+            import functools
+            reduce(spam, ['spam', 'spam', 'baked beans', 'spam'])
+            """
+        self.check(b, a)
+        
+    def test_prefix(self):
+        b = """
+            a  =  functools.reduce( self.thing,  self.children , f( 3 ))
+            """
+        a = """
+            a  =  reduce( self.thing,  self.children , f( 3 ))
+            """
+        self.check(b, a)
