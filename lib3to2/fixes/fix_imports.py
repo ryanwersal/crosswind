46d0aba9d27fbe1d96c3522d2b33f21aa9b3868c
Joe Amenta <airbreather@linux.com>
2009-07-29 02:10:33 -0400

    Massive redux on fix_imports; still misses dotted imports Importing from lib2to3's implementation all that can be re-used, which is almost all of it

--- lib3to2/fixes/fix_imports.py
+++ lib3to2/fixes/fix_imports.py
@@ -2,10 +2,7 @@
 
 import re
 
-from lib2to3 import fixer_base
-from lib2to3.fixer_util import Name, attr_chain
-from lib2to3.pytree import Leaf
-from lib2to3.pgen2 import token
+from lib2to3.fixes.fix_imports import FixImports as FixImports_
 
 MAPPING = {'winreg': '_winreg',
            'configparser': 'ConfigParser',
@@ -39,104 +36,25 @@ MAPPING = {'winreg': '_winreg',
            'xmlrpc.client': 'xmlrpclib',
 }
 
-# This function is just not ready to be included yet.
-# It needs to return a results mapping, but right now it only functions to
-# match dotted import names from the mapping, with terrible efficiency.
+class FixImports(FixImports_):
 
-#TODO: Make this work right
-
-#def match_dotted(node, names, syms):
-#    """Iterate through names looking for dotted modules; try to match in node"""
-#    kids = [str(kid).strip() for kid in node.pre_order()]
-#    for name in names:
-#        if '.' in name:
-#            if name in u''.join(kids):
-#                assert re.match("import *" + name , u''.join(kids))
-#                #This next line needs to return a results mapping of some sort
-#                return {'node':node}
-
-
-def alternates(members):
-    return "(" + "|".join(map(repr, members)) + ")"
-
-
-def build_pattern(mapping=MAPPING):
-    mod_list = ' | '.join(["module_name='%s'" % key for key in mapping])
-    bare_names = alternates(mapping.keys())
-
-    yield """name_import=import_name< 'import' ((%s) |
-               multiple_imports=dotted_as_names< any* (%s) any* >) >
-          """ % (mod_list, mod_list)
-    yield """import_from< 'from' (%s) 'import' ['(']
-              ( any | import_as_name< any 'as' any > |
-                import_as_names< any* >)  [')'] >
-          """ % mod_list
-    yield """import_name< 'import' (dotted_as_name< (%s) 'as' any > |
-               multiple_imports=dotted_as_names<
-                 any* dotted_as_name< (%s) 'as' any > any* >) >
-          """ % (mod_list, mod_list)
-
-    # Find usages of module members in code e.g. thread.foo(bar)
-    yield "power< bare_with_attr=(%s) trailer<'.' any > any* >" % bare_names
-
-
-class FixImports(fixer_base.BaseFix):
-
-    # This is overridden in fix_imports2.
     mapping = MAPPING
 
-    # We want to run this fixer late, so fix_import doesn't try to make stdlib
-    # renames into relative imports.
-    run_order = 6
+    # This function is just not ready to be included yet.
+    # It needs to return a results mapping, but right now it only functions to
+    # match dotted import names from the mapping.
+    
+    #TODO: Make this work right
 
-    def build_pattern(self):
-        return "|".join(build_pattern(self.mapping))
+    def match_dotted(self, node):
+        """Iterate through names trying to match dotted ones"""
+        for name in self.mapping.keys():
+            if '.' in name:
+                if name in str(node):
+                    assert re.match("import *" + name , str(node))
+                    #This next line needs to return a proper results dict
+                    return {'node': node}
 
-    def compile_pattern(self):
-        # We override this, so MAPPING can be pragmatically altered and the
-        # changes will be reflected in PATTERN.
-        self.PATTERN = self.build_pattern()
-        super(FixImports, self).compile_pattern()
-
-    # Don't match the node if it's within another match.
     def match(self, node):
-        match = super(FixImports, self).match
-        #XXX: Uncomment the second part of the next line when match_dotted works
-        results = match(node) #or match_dotted(node, self.mapping.keys(), self.syms)
-        if results:
-            # Module usage could be in the trailer of an attribute lookup, so we
-            # might have nested matches when "bare_with_attr" is present.
-            if "bare_with_attr" not in results and \
-                    any([match(obj) for obj in attr_chain(node, "parent")]):
-                return False
-            return results
-        return False
-
-    def start_tree(self, tree, filename):
-        super(FixImports, self).start_tree(tree, filename)
-        self.replace = {}
-
-    def transform(self, node, results):
-        import_mod = results.get("module_name")
-        if import_mod:
-            mod_name = import_mod.value
-            new_name = unicode(self.mapping[mod_name])
-            import_mod.replace(Name(new_name, prefix=import_mod.prefix))
-            if "name_import" in results:
-                # If it's not a "from x import x, y" or "import x as y" import,
-                # marked its usage to be replaced.
-                self.replace[mod_name] = new_name
-            if "multiple_imports" in results:
-                # This is a nasty hack to fix multiple imports on a line (e.g.,
-                # "import StringIO, urlparse"). The problem is that I can't
-                # figure out an easy way to make a pattern recognize the keys of
-                # MAPPING randomly sprinkled in an import statement.
-                results = self.match(node)
-                if results:
-                    self.transform(node, results)
-        else:
-            # Replace usage of the module.
-            bare_name = results["bare_with_attr"][0]
-            new_name = self.replace.get(bare_name.value)
-            if new_name:
-                bare_name.replace(Name(new_name, prefix=bare_name.prefix))
+        #XXX Uncomment the second part of the next line when match_dotted works
+        return super(FixImports, self).match(node) #or match_dotted(node)
