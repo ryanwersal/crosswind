6e8d2db2fdafb8c5b5dc620570668fd0d633085c
Joe Amenta <airbreather@linux.com>
2009-07-29 04:52:43 -0400

    fixes for dotted imports are almost complete The import line is fixed, but usage in the rest of the code is not.

--- lib3to2/fixes/fix_imports.py
+++ lib3to2/fixes/fix_imports.py
@@ -2,7 +2,21 @@
 
 import re
 
+from lib2to3.pygram import python_symbols as syms
+from lib2to3.pytree import Node
+from lib2to3.fixer_util import Name, Dot, Attr
+
 from lib2to3.fixes.fix_imports import FixImports as FixImports_
+from lib2to3.fixes.fix_imports import build_pattern
+
+def DottedName(names, prefix=u""):
+    """Accepts a sequence of names; returns a DottedName that combines them"""
+    children = []
+    for arg in names:
+        children.append(Name(arg))
+        children.append(Dot())
+    del children[-1]
+    return Node(syms.dotted_name, children, prefix)
 
 MAPPING = {'winreg': '_winreg',
            'configparser': 'ConfigParser',
@@ -40,21 +54,37 @@ class FixImports(FixImports_):
 
     mapping = MAPPING
 
-    # This function is just not ready to be included yet.
-    # It needs to return a results mapping, but right now it only functions to
-    # match dotted import names from the mapping.
-    
-    #TODO: Make this work right
+    def find_usage(self, node, ref):
+        """Returns an object in node's children that is equivalent to ref"""
+        for child in node.pre_order():
+            # Discard differences in prefixes
+            ref.prefix = child.prefix
+            if child == ref:
+                return child
 
+    # TODO: Add support for fixing later usage for name_import cases.
     def match_dotted(self, node):
-        """Iterate through names trying to match dotted ones"""
+        """Iterate through names matching the dotted ones"""
+        results = {"node": node}
+        matched = []
         for name in self.mapping.keys():
             if '.' in name:
-                if name in str(node):
-                    assert re.match("import *" + name , str(node))
-                    #This next line needs to return a proper results dict
-                    return {'node': node}
+                attrs = [Name(attr) for attr in name.split('.')]
+                import_mod = self.find_usage(node, DottedName(name.split('.')))
+                if import_mod:
+                    import_mod.value = name
+                    results["module_name"] = import_mod
+                    matched.append(import_mod)
+        # This switch flags self.transform to perform certain actions
+        if len(matched) == 0:
+            return False
+        if len(matched) == 1:
+            if not re.match("from %s import +" % name, str(node)) or \
+               not re.match("import * %s * as +" % name, str(node)):
+                results["name_import"] = True
+        else:
+            results["multiple_imports"] = True
+        return results
 
     def match(self, node):
-        #XXX Uncomment the second part of the next line when match_dotted works
-        return super(FixImports, self).match(node) #or match_dotted(node)
+        return super(FixImports, self).match(node) or self.match_dotted(node)
