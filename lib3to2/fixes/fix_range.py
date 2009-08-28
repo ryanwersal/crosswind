436a4507acc21fcdc3f5b71ee1188e3ae2bb4446
Joe Amenta <airbreather@linux.com>
2009-08-28 05:46:36 -0400

    Added list(range(s)) -> range(s) to fix_range

--- lib3to2/fixes/fix_range.py
+++ lib3to2/fixes/fix_range.py
@@ -1,18 +1,47 @@
 """
-Fixer for range(s) -> xrange(s).
+Fixer for:
+range(s) -> xrange(s)
+list(range(s)) -> range(s)
 """
 
 from lib2to3 import fixer_base
-from lib2to3.fixer_util import Name
+from lib2to3.fixer_util import Name, is_probably_builtin
+from lib2to3.pygram import python_symbols as syms
+import token
+
+def list_called(node):
+    """
+    Returns the power node that contains list as its first child if node
+    is contained in a list() call, otherwise False.
+    """
+    parent = node.parent
+    if parent is not None and parent.type == syms.trailer:
+        prev = parent.prev_sibling
+        if prev is not None and \
+           prev.type == token.NAME and \
+           prev.value == u"list" and \
+           is_probably_builtin(prev):
+            return prev.parent
+    return False
 
 class FixRange(fixer_base.BaseFix):
 
     PATTERN = """
-              power<
-                 (name='range') trailer< '(' args=any ')' >
-              rest=any* >
+              power< name='range' trailer< '(' any ')' > >
               """
 
     def transform(self, node, results):
         name = results["name"]
-        name.replace(Name(u"xrange", prefix=name.prefix))
+        if not is_probably_builtin(name):
+            return
+        list_call = list_called(node)
+        if list_call:
+            new_node = node.clone()
+            new_node.prefix = list_call.prefix
+            parent = list_call.parent
+            i = list_call.remove()
+            for after in list_call.children[2:]:
+                new_node.append_child(after)
+            parent.insert_child(i, new_node)
+        else:
+            name.replace(Name(u"xrange", prefix=name.prefix))
--- lib3to2/tests/test_range.py
+++ lib3to2/tests/test_range.py
@@ -3,6 +3,11 @@ from test_all_fixers import lib3to2FixerTestCase
 class Test_range(lib3to2FixerTestCase):
     fixer = "range"
 
+    def test_notbuiltin_list(self):
+        b = "x.list(range(10))"
+        a = "x.list(xrange(10))"
+        self.check(b, a)
+
     def test_prefix_preservation(self):
         b = """x =    range(  10  )"""
         a = """x =    xrange(  10  )"""
@@ -31,28 +36,26 @@ class Test_range(lib3to2FixerTestCase):
         a = """x = xrange(0, 10, 2)"""
         self.check(b, a)
 
-        #XXX TODO: Make this work!
+    def test_wrapped_in_list(self):
+        b = """x = list(range(10, 3, 9))"""
+        a = """x = range(10, 3, 9)"""
+        self.check(b, a)
 
-#    def test_wrapped_in_list(self):
-#        b = """x = list(range(10, 3, 9))"""
-#        a = """x = range(10, 3, 9))"""
-#        self.check(b, a)
+        b = """x = foo(list(range(10, 3, 9)))"""
+        a = """x = foo(range(10, 3, 9))"""
+        self.check(b, a)
 
-#        b = """x = foo(list(range(10, 3, 9)))"""
-#        a = """x = foo(range(10, 3, 9))"""
-#        self.check(b, a)
+        b = """x = list(range(10, 3, 9)) + [4]"""
+        a = """x = range(10, 3, 9) + [4]"""
+        self.check(b, a)
 
-#        b = """x = list(range(10, 3, 9)) + [4]"""
-#        a = """x = range(10, 3, 9) + [4]"""
-#        self.check(b, a)
+        b = """x = list(range(10))[::-1]"""
+        a = """x = range(10)[::-1]"""
+        self.check(b, a)
 
-#        b = """x = list(range(10))[::-1]"""
-#        a = """x = range(10)[::-1]"""
-#        self.check(b, a)
-
-#        b = """x = list(range(10))  [3]"""
-#        a = """x = range(10)  [3]"""
-#        self.check(b, a)
+        b = """x = list(range(10))  [3]"""
+        a = """x = range(10)  [3]"""
+        self.check(b, a)
 
     def test_range_in_for(self):
         b = """for i in range(10):\n    j=i"""
