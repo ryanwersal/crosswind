3ced86a38e68abff3563b376fccfdace7fabb504
Joe Amenta <airbreather@linux.com>
2009-09-01 00:57:41 -0400

    Fixing issue #1

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from distutils.core import setup
 
 setup(
    name="3to2",
-   packages=['lib3to2','lib3to2.fixes','lib3to2.tests'],
-   scripts=["3to2"]
+   packages=["lib3to2","lib3to2.fixes","lib3to2.tests"],
+   scripts=["3to2"],
+   version="0.1a1",
 )
