308eab533fc2b64fde8b46bc36686fb58b62e0a4
Joe Amenta <airbreather@linux.com>
2009-09-01 01:06:44 -0400

    Fixing issue #2 setup.py now fails to build on versions < 2.7 and >= 3.0

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python2.7
+
+#Thanks to srid for this next bit:
+import sys
+print ("Checking Python version info..."),
+if sys.version_info < (2, 7) or sys.version_info >= (3, 0):
+    sys.exit("ERROR: 3to2 requires at least Python 2.7 in the 2.x branch.")
+else:
+    print ("%d.%d.%d" % (sys.version_info[:3]))
+
 from distutils.core import setup
 
 setup(
