7b70b2badbfbb2573b6adffe95176a08d567d7ae
Joe Amenta <airbreather@linux.com>
2009-09-17 08:58:03 -0400

    Administrative stuff: it looks like I'm going to be managing this with distutils, so I've added more descriptive tags.

--- setup.py
+++ setup.py
@@ -8,6 +8,17 @@ if sys.version_info < (2, 7) or sys.version_info >= (3, 0):
 else:
     print ("%d.%d.%d" % (sys.version_info[:3]))
 
+classifiers = [
+"Development Status :: 3 - Alpha",
+"Environment :: Console",
+"Intended Audience :: Developers",
+"License :: OSI Approved :: Apache Software License",
+"Operating System :: OS Independent",
+"Programming Language :: Python :: 2.7",
+"Topic :: Software Development :: Code Generators",
+"Topic :: Software Development :: Libraries :: Python Modules",
+]
+
 from distutils.core import setup
 
 setup(
@@ -15,4 +26,12 @@ setup(
    packages=["lib3to2","lib3to2.fixes","lib3to2.tests"],
    scripts=["3to2"],
    version="0.1a2",
+   url="http://www.startcodon.com/wordpress/?cat=8",
+   author="Joe Amenta",
+   author_email="amentajo@msu.edu",
+   classifiers=classifiers,
+   description="Refactors valid 3.x syntax into valid 2.x syntax, if a syntactical conversion is possible",
+   long_description="",
+   license="",
+   platforms="",
 )
