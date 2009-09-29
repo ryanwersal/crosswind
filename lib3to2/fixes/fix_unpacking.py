b48b24bd7a17ca2e43e9bbfff1b246445e6d4dd9
Joe Amenta <airbreather@linux.com>
2009-09-29 14:29:31 -0400

    Readability and more stubs Honoring 80-char lines.

--- lib3to2/fixes/fix_unpacking.py
+++ lib3to2/fixes/fix_unpacking.py
@@ -8,16 +8,44 @@ from lib2to3 import fixer_base
 from lib2to3.pytree import Node, Leaf
 from lib2to3.pygram import token, python_symbols as syms
 
+LISTNAME = u"_3to2list"
+ITERNAME = u"_3to2iter"
+
+def assignment_stmt(pre, starg, post):
+    """
+    Accepts a list of mandatory argument names before the star_expr,
+    the star_expr name itself,
+    and a list of mandatory argument names after the star_expr
+    Returns a node of the form"
+    pre, starg, post = _3to2iter[:len(pre)] + \
+                      [_3to2iter[len(pre):-len(post)]] + \
+                       _3to2iter[-len(post):]
+    """
+    pass
+
 class FixUnpacking(fixer_base.BaseFix):
 
     PATTERN = """
-    expr_stmt< testlist_star_expr< pre=(any ',')* star_expr< '*' name=NAME > post=(',' any)* [','] > '=' target=any > |
-    for_stmt< 'for' exprlist< pre=(any ',')* star_expr< '*' name=NAME > post=(',' any)* [','] > 'in' it=any ':' suite=any>"""
+    expl=expr_stmt< testlist_star_expr<
+        pre=(any ',')*
+            star_expr< '*' name=NAME >
+        post=(',' any)* [','] > '=' from=any > |
+    impl=for_stmt< 'for' exprlist<
+        pre=(any ',')*
+            star_expr< '*' name=NAME >
+        post=(',' any)* [','] > 'in' it=any ':' suite=any>"""
+
+    def fix_explicit_context(self, node, results):
+        pass
+
+    def fix_implicit_context(self, node, results):
+        pass
 
     def transform(self, node, results):
         """
         a,b,c,d,e,f,*g,h,i = range(100) changes to
-        _3to2list=list(range(100));a,b,c,d,e,f,g,h,i = _3to2iter[:6] + [_3to2iter[6:-2]] + _3to2iter[-2:]
+        _3to2list=list(range(100))
+        a,b,c,d,e,f,g,h,i = _3to2iter[:6] + [_3to2iter[6:-2]] + _3to2iter[-2:]
 
         and
 
@@ -27,4 +55,6 @@ class FixUnpacking(fixer_base.BaseFix):
             a,b,c,d,e = _3to2list[:2] + [_3to2list[2:-2]] + _3to2list[-2:]
             do_stuff
         """
-        # implementation goes here...
+        expl, impl = results.get("expl"), results.get("impl")
+        return self.fix_implicit_context(node, results) if impl is not None \
+          else self.fix_explicit_context(node, results)
