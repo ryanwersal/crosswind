4af711899cc45e6ad38417288600d1e5c431784a
Joe Amenta <airbreather@linux.com>
2009-10-06 18:59:56 -0400

    unpacking fix works in more cases

--- lib3to2/fixes/fix_unpacking.py
+++ lib3to2/fixes/fix_unpacking.py
@@ -7,7 +7,7 @@ for (a,)* *b (,c)* [,] in d: ...
 from lib2to3 import fixer_base
 from lib2to3.pytree import Node, Leaf
 from lib2to3.pygram import token, python_symbols as syms
-from lib2to3.fixer_util import Assign, Call, Newline, Name, Number
+from lib2to3.fixer_util import Assign, Comma, Call, Newline, Name, Number
 from fix_imports2 import commatize
 
 LISTNAME = u"_3to2list"
@@ -19,8 +19,21 @@ def assignment_source(num_pre, num_post):
     before and after the starg (not including the starg)
     Returns a source fit for Assign() from fixer_util
     """
-    # Ugly line... TODO: clean it up for readability
-    source = Node(syms.arith_expr, [Node(syms.power, [Name(LISTNAME), Node(syms.trailer, [Leaf(token.LSQB, u"["), Node(syms.subscript, [Leaf(token.COLON, u":"), Number(num_pre)]), Leaf(token.RSQB, u"]")])]), Leaf(token.PLUS, u"+", prefix=u" "), Node(syms.power, [Leaf(token.LSQB, u"[", prefix=u" "), Name(LISTNAME), Node(syms.trailer, [Leaf(token.LSQB, u"["), Node(syms.subscript, [Number(num_pre), Leaf(token.COLON, u":"), Node(syms.factor, [Leaf(token.MINUS, u"-"), Number(num_post)])]), Leaf(token.RSQB, u"]"), Leaf(token.RSQB, u"]")])]), Leaf(token.PLUS, u"+", prefix=u" "), Node(syms.power, [Name(LISTNAME, prefix=u" "), Node(syms.trailer, [Leaf(token.LSQB, u"["), Node(syms.subscript, [Node(syms.factor, [Leaf(token.MINUS, u"-"), Number(num_post)]), Leaf(token.COLON, u":")]), Leaf(token.RSQB, u"]")])])])
+    children = []
+    pre = str(num_pre)
+    post = str(num_post)
+    # a little prettier... still some cleanup TODO
+    if num_pre > 0:
+        pre_part = Node(syms.power, [Name(LISTNAME), Node(syms.trailer, [Leaf(token.LSQB, u"["), Node(syms.subscript, [Leaf(token.COLON, u":"), Number(pre)]), Leaf(token.RSQB, u"]")])])
+        children.append(pre_part)
+        children.append(Leaf(token.PLUS, u"+", prefix=u" "))
+    main_part = Node(syms.power, [Leaf(token.LSQB, u"[", prefix=u" "), Name(LISTNAME), Node(syms.trailer, [Leaf(token.LSQB, u"["), Node(syms.subscript, [Number(pre) if num_pre > 0 else Leaf(1, u""), Leaf(token.COLON, u":"), Node(syms.factor, [Leaf(token.MINUS, u"-"), Number(post)]) if num_post > 0 else Leaf(1, u"")]), Leaf(token.RSQB, u"]"), Leaf(token.RSQB, u"]")])])
+    children.append(main_part)
+    if num_post > 0:
+        children.append(Leaf(token.PLUS, u"+", prefix=u" "))
+        post_part = Node(syms.power, [Name(LISTNAME, prefix=u" "), Node(syms.trailer, [Leaf(token.LSQB, u"["), Node(syms.subscript, [Node(syms.factor, [Leaf(token.MINUS, u"-"), Number(post)]), Leaf(token.COLON, u":")]), Leaf(token.RSQB, u"]")])])
+        children.append(post_part)
+    source = Node(syms.arith_expr, children)
     return source
 
 class FixUnpacking(fixer_base.BaseFix):
@@ -37,11 +50,13 @@ class FixUnpacking(fixer_base.BaseFix):
 
     def fix_explicit_context(self, node, results):
         pre, name, post, source = results.get("pre"), results.get("name"), results.get("post"), results.get("source")
-        assert all((pre, name, post, source)), repr(node)
         pre = [n.clone() for n in pre if n.type == token.NAME]
         name.prefix = u" "
         post = [n.clone() for n in post if n.type == token.NAME]
         target = [n.clone() for n in commatize(pre + [name.clone()] + post)]
+        # to make the special-case fix for "*z, = ..." correct with the least
+        # amount of modification, make the left-side into a guaranteed tuple
+        target.append(Comma())
         source.prefix = u""
         setup_line = Assign(Name(LISTNAME), Call(Name(u"list"), [source.clone()]))
         power_line = Assign(target, assignment_source(len(pre), len(post)))
@@ -53,7 +68,7 @@ class FixUnpacking(fixer_base.BaseFix):
     def transform(self, node, results):
         """
         a,b,c,d,e,f,*g,h,i = range(100) changes to
-        _3to2list=list(range(100))
+        _3to2list = list(range(100))
         a,b,c,d,e,f,g,h,i = _3to2iter[:6] + [_3to2iter[6:-2]] + _3to2iter[-2:]
 
         and
@@ -68,6 +83,8 @@ class FixUnpacking(fixer_base.BaseFix):
         if expl is not None:
             setup_line, power_line = self.fix_explicit_context(node, results)
             setup_line.prefix = expl.prefix
+            setup_line.append_child(Newline())
+            power_line.prefix = u"" # XXX: Incorrect in suites.
             parent = node.parent
             i = node.remove()
             parent.insert_child(i, power_line)
--- lib3to2/tests/test_unpacking.py
+++ lib3to2/tests/test_unpacking.py
@@ -20,7 +20,7 @@ class Test_unpacking(lib3to2FixerTestCase):
         a = """
         for _3to2iter in two_dim_array:
             _3to2list = list(_3to2iter)
-            a, b, c, d, e = _3to2list[:3] + [_3to2list[3:-1]] + _3to2list[-1:]
+            a, b, c, d, e, = _3to2list[:3] + [_3to2list[3:-1]] + _3to2list[-1:]
             pass"""
         self.check(b, a)
 
@@ -30,7 +30,7 @@ class Test_unpacking(lib3to2FixerTestCase):
         a = """
         for _3to2iter in some_thing:
             _3to2list = list(_3to2iter)
-            a, b, c = _3to2list[:2] + [_3to2list[2:]]
+            a, b, c, = _3to2list[:2] + [_3to2list[2:]]
             do_stuff"""
         self.check(b, a)
 
@@ -40,7 +40,7 @@ class Test_unpacking(lib3to2FixerTestCase):
         a = """
         for _3to2iter in some_thing:
             _3to2list = list(_3to2iter)
-            a, b, c, d, e, f, g = [_3to2list[:-6]] + _3to2list[-6:]
+            a, b, c, d, e, f, g, = [_3to2list[:-6]] + _3to2list[-6:]
             pass"""
         self.check(b, a)
 
@@ -49,32 +49,33 @@ class Test_unpacking(lib3to2FixerTestCase):
         a, *b, c = range(100)"""
         a = """
         _3to2list = list(range(100))
-        a, b, c = _3to2list[:1] + [_3to2list[1:-1]] + _3to2list[-1:]"""
+        a, b, c, = _3to2list[:1] + [_3to2list[1:-1]] + _3to2list[-1:]"""
         self.check(b, a)
 
         b = """
         a, b, c, d, *e, f, g = letters"""
         a = """
         _3to2list = list(letters)
-        a, b, c, d, e, f, g = _3to2list[:4] + [_3to2list[4:-2]] + _3to2list[-2:]"""
+        a, b, c, d, e, f, g, = _3to2list[:4] + [_3to2list[4:-2]] + _3to2list[-2:]"""
         self.check(b, a)
 
         b = """
         *e, f, g = letters"""
         a = """
         _3to2list = list(letters)
-        e, f, g = [_3to2list[:-2]] + _3to2list[-2:]"""
+        e, f, g, = [_3to2list[:-2]] + _3to2list[-2:]"""
         self.check(b, a)
 
         b = """
         a, b, c, d, *e = stuff"""
         a = """
         _3to2list = list(stuff)
-        a, b, c, d, e = _3to2list[:4] + [_3to2list[4:]]"""
+        a, b, c, d, e, = _3to2list[:4] + [_3to2list[4:]]"""
         self.check(b, a)
 
         b = """
         *z, = stuff"""
         a = """
-        z = list(stuff)"""
+        _3to2list = list(stuff)
+        z, = [_3to2list[:]]"""
         self.check(b, a)
