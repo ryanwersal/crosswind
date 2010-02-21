3fcd73fb5ce72cecb4ac2ddcbd19385f5535b6eb
Joe Amenta <airbreather@linux.com>
2010-02-21 12:43:10 -0500

    Warning about function annotations when removing them

--- lib3to2/fixes/fix_annotations.py
+++ lib3to2/fixes/fix_annotations.py
@@ -6,11 +6,20 @@ from lib2to3 import fixer_base
 from lib2to3.pgen2 import token
 from lib2to3.fixer_util import syms
 
+warning_text = "Removing function annotations completely."
+
 def param_without_annotations(node):
     return node.children[0]
 
 class FixAnnotations(fixer_base.BaseFix):
 
+    warned = False
+
+    def warn_once(self, node, reason):
+        if not self.warned:
+            self.warned = True
+            self.warning(node, reason=reason)
+    
     PATTERN = """
               funcdef< 'def' any parameters< '(' [params=any] ')' > ['->' ret=any] ':' any* >
               """
@@ -23,14 +32,17 @@ class FixAnnotations(fixer_base.BaseFix):
         ret = results.get("ret")
         if ret is not None:
             assert ret.prev_sibling.type == token.RARROW, "Invalid return annotation"
+            self.warn_once(node, reason=warning_text)
             ret.prev_sibling.remove()
             ret.remove()
         if params is None: return
         if params.type == syms.typedargslist:
             # more than one param in a typedargslist
+            self.warn_once(node, reason=warning_text)
             for param in params.children:
                 if param.type == syms.tname:
                     param.replace(param_without_annotations(param))
         elif params.type == syms.tname:
             # one param
+            self.warn_once(node, reason=warning_text)
             params.replace(param_without_annotations(params))
