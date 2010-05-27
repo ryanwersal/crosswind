a14a294578341213031810812c2de2b77e3572f8
Joe Amenta <airbreather@linux.com>
2010-05-27 15:37:02 -0400

    Adding tests to drive fix_imports2 development. This is going to be really hard, as more elements of semantics are needed than I initially thought. As it is, I've already decided that a few things that would be prohibitively difficult to implement.

--- lib3to2/tests/test_imports2.py
+++ lib3to2/tests/test_imports2.py
@@ -3,7 +3,158 @@ from test_all_fixers import lib3to2FixerTestCase
 class Test_imports2(lib3to2FixerTestCase):
     fixer = "imports2"
 
-    def test_simple_imports(self):
+    def test_def_scope(self):
+
+        b = """
+        import urllib.request
+        def importing_stuff():
+            import urllib.request
+            urllib.request.urlopen(stuff)
+        urllib.request.urlretrieve(stuff)"""
+        a = """
+        import urllib
+        def importing_stuff():
+            import urllib2
+            urllib2.urlopen(stuff)
+        urllib.urlretrieve(stuff)"""
+        self.check(b, a)
+
+
+    def test_class_scope(self):
+
+        b = """
+        import http.server
+
+        class StuffDoer(object):
+            import http.server
+
+            def __init__(self):
+                # class namespace search after instance namespace lookup fails
+                self.http.server.HTTPServer(('localhost','80'), http.server.SimpleHTTPRequestHandler)
+
+        class OtherStuffDoer(StuffDoer):
+            def serve(self):
+                # base class namespace search after derived class namespace lookup fails
+                self.http.server.HTTPServer(('localhost', '8080'), self.http.server.CGIHTTPRequestHandler)
+
+        http.server.HTTPServer(('localhost', '8000'), http.server.BaseHTTPRequestHandler)"""
+        a = """
+        import BaseHTTPServer
+        import SimpleHTTPServer
+
+        class StuffDoer(object):
+
+            import BaseHTTPServer
+            import CGIHTTPServer
+
+            def __init__(self):
+                # class namespace search after instance namespace lookup fails
+                self.BaseHTTPServer.HTTPServer(('localhost', '80'), SimpleHTTPServer.SimpleHTTPRequestHandler)
+
+        class OtherStuffDoer(StuffDoer):
+
+            def serve(self):
+                # base class namespace search after derived class namespace lookup fails
+                self.BaseHTTPServer.HTTPServer(('localhost', '8080'), self.CGIHTTPServer.CGIHTTPRequestHandler)
+
+        BaseHTTPServer.HTTPServer(('localhost', '8080'), BaseHTTPServer.BaseHTTPRequestHandler)"""
+
+        # This one might be impossible to implement completely.
+        # It's also probably unnecessary and definitely impractical.
+        # One obvious limitation is that it only could ever hope to do the
+        # right thing if all the classes are defined in one module.
+
+        # self.check(b, a)
+    
+    def test_if_else_scope(self):
+        
+        b = """
+        if thing:
+            import http.server
+        elif other_thing:
+            import xmlrpc.server
+        if related_thing:
+            myServ = http.server.HTTPServer(('localhost', '80'), http.server.CGIHTTPRequestHandler)
+        elif other_related_thing:
+            myServ = xmlrpc.server.SimpleXMLRPCServer(('localhost', '80'), CGIXMLRPCRequestHandler)
+
+        # just for kicks...
+        monkey_wrench_in_the_works = http.server.SimpleHTTPRequestHandler"""
+
+        a = """
+        if thing:
+            import BaseHTTPServer
+            import CGIHTTPServer
+            import SimpleHTTPServer
+        elif other_thing:
+            import SimpleXMLRPCServer
+        if related_thing:
+            myServ = BaseHTTPServer.HTTPServer(('localhost', '80'), CGIHTTPServer.CGIHTTPRequestHandler)
+        elif other_related_thing:
+            myServ = SimpleXMLRPCServer.SimpleXMLRPCServer(('localhost', '80'), CGIXMLRPCRequestHandler)
+
+        # just for kicks...
+        monkey_wrench_in_the_works = SimpleHTTPServer.SimpleHTTPRequestHandler"""
+        self.check(b, a)
+
+    def test_try_except_scope(self):
+        
+        b = """
+        try:
+            import http.server
+        except ImportError:
+            import xmlrpc.server
+
+        # some time has passed, and we know that http.server was bad.
+        srv = xmlrpc.server.DocXMLRPCServer(addr, xmlrpc.server.DocCGIXMLRPCRequestHandler)
+
+        # some more time has passed, and we know that http.server is good.
+        srv = http.server.HTTPServer(addr, http.server.CGIHTTPRequestHandler)"""
+
+        a = """
+        try:
+            import BaseHTTPServer
+            import CGIHTTPServer
+        except ImportError:
+            import DocXMLRPCServer
+
+        # some time has passed, and we know that http.server was bad.
+        srv = DocXMLRPCServer.DocXMLRPCServer(addr, DocXMLRPCServer.DocCGIXMLRPCRequestHandler)
+
+        # some more time has passed, and we know that http.server is good.
+        srv = BaseHTTPServer.HTTPServer(addr, CGIHTTPServer.CGIHTTPRequestHandler)"""
+        self.check(b, a)
+
+    def test_try_except_conditional(self):
+        
+        b = """
+        try:
+            import http.server as s
+        except ImportError:
+            import xmlrpc.server as s
+
+        srv = s.DocXMLRPCServer(addr, s.DocCGIXMLRPCRequestHandler)
+
+        # some more time has passed, and we know that http.server is good.
+        srv = s.HTTPServer(addr, s.BaseHTTPRequestHandler)"""
+
+        a = """
+        try:
+            import BaseHTTPServer as s
+        except ImportError:
+            import DocXMLRPCServer as s
+
+        # some time has passed, and we know that http.server was bad.
+        srv = s.DocXMLRPCServer(addr, s.DocCGIXMLRPCRequestHandler)
+
+        # some more time has passed, and we know that http.server is good.
+        srv = s.HTTPServer(addr, s.BaseHTTPRequestHandler)"""
+        # Don't expect this to work.  It requires semantics checking, and it
+        # would be limited to py3k library module imports that only use a
+        # subset of the members that are provided by a single py2k module.
+        # self.check(b, a)
+
+    def test_import_names(self):
 
         b = "from urllib.request import urlopen"
         a = "from urllib2 import urlopen"
@@ -77,7 +228,7 @@ class Test_imports2(lib3to2FixerTestCase):
             fileList = [ln for ln in myFile]"""
         self.check(b, a, ignore_warnings=True)
 
-    def test_alt_wording(self):
+    def test_from_import_module(self):
 
         b = """
         if spam.is_good():
