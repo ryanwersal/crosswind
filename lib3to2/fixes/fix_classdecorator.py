74107e183a58bc85228e80fb1c0b7e6f40b470a8
Joe Amenta <airbreather@linux.com>
2010-06-24 11:46:29 -0400

    Very hacky implementation of the classdecorator fix. Fails tests because of a difference in the position of newlines.

--- lib3to2/fixes/fix_classdecorator.py
+++ lib3to2/fixes/fix_classdecorator.py
@@ -3,16 +3,8 @@ Fixer to remove class decorators
 """
 
 from lib2to3 import fixer_base
-from lib2to3.fixer_util import Call, Assign
-
-def dec_name(decorator_node):
-    return decorator_node.children[1]
-
-def get_pos(node):
-    assert node.parent, repr(node)
-    for i, child in enumerate(node.parent.children):
-        if child is node:
-            return i
+from lib2to3.fixer_util import Call, Assign, String, Newline
+from ..fixer_util import Leaf, Node, token, syms, indentation
 
 def call_chain(inner_name, names):
     """
@@ -30,29 +22,30 @@ def call_chain(inner_name, names):
 class FixClassdecorator(fixer_base.BaseFix):
 
     PATTERN = """
-              dec_node=decorated < dec=decorator < any* > cls=classdef < 'class' name=any any* > > | 
-              dec_node=decorated < decs=decorators < any* > cls=classdef < 'class' name=any any* > >
+              decorated < one_dec=decorator < any* > cls=classdef < 'class' name=any any* > > | 
+              decorated < decorators < decs=decorator+ > cls=classdef < 'class' name=any any* > >
               """
-
     def transform(self, node, results):
-        """
-        This removes the decorators and replaces them
-        with the syntactical equivalent (source: PEP 3129)        
-        """
-        dec_node = results["dec_node"]
-        cls = results["cls"]
-        name = results["name"].clone()
-        name.prefix = ""
-        # If there's just one decorator, put it in a list
-        # Otherwise, get all the decorators into one list
-        decs = [results.get("dec")] if "dec" in results else \
-               [dec for dec in results.get("decs").children]
-        # where to insert the new name after decorations are removed
-        pos = get_pos(cls)+1
-        dec_names = [dec_name(dec).clone() for dec in decs]
+
+        singleton = results.get("one_dec")
+        decs = [results["one_dec"]] if results.get("one_dec") is not None else results["decs"]
+        dec_strings = [str(dec).strip()[1:] for dec in decs]
+        assign = ""
+        for dec in dec_strings:
+            assign += dec
+            assign += "("
+        assign += results["name"].value
+        for dec in dec_strings:
+            assign += ")"
+        assign = String(results["name"].value + " = " + assign)
+        assign_statement = Node(syms.simple_stmt, [assign, Newline()])
+        prefix = None
         for dec in decs:
+            if prefix is None:
+                prefix = dec.prefix
             dec.remove()
-        # new_name becomes first_decorator(second_decorator(...(class_name)...))
-        new_name = call_chain(name, dec_names)
-        new_assignment = Assign(name, new_name)
-        cls.parent.insert_child(pos, new_assignment)
+        results["cls"].prefix = prefix
+        pos = node.children.index(results["cls"]) + 1
+        i = indentation(node)
+        node.insert_child(pos, Leaf(token.INDENT, i))
+        node.insert_child(pos, assign_statement)
