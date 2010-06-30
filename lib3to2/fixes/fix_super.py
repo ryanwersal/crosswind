c76f258468bf581e213d958a313a1319d5402b11
Joe Amenta <airbreather@linux.com>
2010-06-29 21:10:20 -0400

    Failing a little more gently when super() is called with no arguments in some wrong cases Case 1: super() called outside of a funcdef Case 2: super() called inside a funcdef that has no parameters

--- lib3to2/fixes/fix_super.py
+++ lib3to2/fixes/fix_super.py
@@ -21,12 +21,16 @@ def get_firstparam(super_node):
         parent = parent.parent
 
     if parent.type != syms.funcdef:
+        # super() called without arguments outside of a funcdef
         return None
 
     children = parent.children
     assert len(children) > 2
     params = children[2]
-    assert params.type == syms.parameters and len(params.children) > 2
+    assert params.type == syms.parameters
+    if len(params.children) < 3:
+        # Function has no parameters, therefore super() makes no sense here...
+        return None
     name = params.children[1]
     assert name.type == token.NAME
     return name.value
@@ -45,5 +49,8 @@ class FixSuper(fixer_base.BaseFix):
 
     def transform(self, node, results):
         param = get_firstparam(node)
+        if param is None:
+            self.cannot_convert(node, "super() with no arguments must be called inside a function that has at least one parameter")
+            return
         rparen = results["rparen"]
         insert_args(param, rparen)
