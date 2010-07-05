245503d3ae0c623d351cec29edc5d666be831583
Joe Amenta <airbreather@linux.com>
2010-07-04 22:54:38 -0400

    Changing builtin "open()" to "io.open()"

---
+++ lib3to2/tests/test_open.py
@@ -0,0 +1,13 @@
+from test_all_fixers import lib3to2FixerTestCase
+
+class Test_open(lib3to2FixerTestCase):
+    fixer = "open"
+
+    def test_imports(self):
+        b = """new_file = open("some_filename", newline="\\r")"""
+        a = """from io import open\nnew_file = open("some_filename", newline="\\r")"""
+        self.check(b, a)
+
+    def test_doesnt_import(self):
+        s = """new_file = nothing.open("some_filename")"""
+        self.unchanged(s)
