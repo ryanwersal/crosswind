ed4c5c34620c16497c69072d878a773052cc03e8
Shawn Brown <devnull@localhost>
2013-01-05 00:37:56 -0500

    Fixing path separator handling in test_all_fixers.py to work on Windows as well as Linux.

--- test_all_fixers.py
+++ test_all_fixers.py
@@ -8,12 +8,11 @@ import os
 import importlib
 import lib3to2.tests.support as support
 
-tests_directory = 'lib3to2/tests'  # Relative path to test directory.
+tests_package = 'lib3to2.tests'
 
 if __name__ == "__main__":
-    for module in os.listdir(tests_directory):
+    for module in os.listdir(tests_package.replace('.', os.sep)):
         if module.endswith('.py') and module.startswith('test_'):
-            module = os.path.join(tests_directory, module[:-3])
-            module = module.replace(os.sep, '.')
+            module = tests_package + '.' + module[:-3]
             _module = importlib.import_module(module)
             support.run_all_tests(_module)
