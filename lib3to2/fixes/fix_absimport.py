0f17ea0a4301e7d84360a1be7ab640fa6337754d
Steven Silvester <steven.silvester@ieee.org>
2013-10-16 19:44:38 -0500

    Adding absolute import coverage from awkwardduet

---
+++ lib3to2/fixes/fix_absimport.py
@@ -0,0 +1,29 @@
+'''
+Add 'from __future__ import absolute_import' to any file
+that uses imports.
+'''
+from lib2to3 import fixer_base
+from lib2to3.pygram import python_symbols as syms
+from lib3to2.fixer_util import future_import
+
+class FixAbsimport(fixer_base.BaseFix):
+    order = 'post'
+    
+    def __init__(self, options, log):
+        super(FixAbsimport, self).__init__(options, log)
+        self.__abs_added = None
+        
+    def start_tree(self, tree, filename):
+        super(FixAbsimport, self).start_tree(tree, filename)
+        self.__abs_added = False
+        
+    def match(self, node):
+        return (node.type in (syms.import_name, syms.import_from) 
+                and not self.__abs_added)
+    
+    def transform(self, node, results):
+        future_import('absolute_import', node)
+        self.__abs_added = True
+        
+    def finish_tree(self, tree, filename):
+        fixer_base.BaseFix.finish_tree(self, tree, filename)
