d085b2f78dec49139dd0a0721fa8acb9597b7161
Ryan Wersal <ryan@ryanwersal.com>
2019-10-23 10:39:56 -0500

    Target 2to3 fix_basestring at the isinstance case
    Adds tests for cases that were identified as being changed
    when it wasn't desirable (futurize, etc).

--- fixer_suites/two_to_three/fixes/fix_basestring.py
+++ fixer_suites/two_to_three/fixes/fix_basestring.py
@@ -3,13 +3,16 @@
 
 # Local imports
 from crosswind import fixer_base
-from crosswind.fixer_util import Name
+from crosswind.fixer_util import Call, Comma, Name, ArgList
 
 
 class FixBasestring(fixer_base.BaseFix):
     BM_compatible = True
 
-    PATTERN = "'basestring'"
+    PATTERN = "power< 'isinstance' trailer< '(' args=arglist< any ',' basestring='basestring' > ')' > >"
 
     def transform(self, node, results):
-        return Name("str", prefix=node.prefix)
+        args = results["args"]
+        args.children[-1].replace(Name("str", prefix=results["basestring"].prefix))
+        args = [a.clone() for a in args.children]
+        return Call(Name("isinstance"), args=args, prefix=node.prefix)
--- fixer_suites/two_to_three/tests/test_basestring.py
+++ fixer_suites/two_to_three/tests/test_basestring.py
@@ -8,3 +8,15 @@ class Test_basestring(FixerTestCase):
         b = """isinstance(x, basestring)"""
         a = """isinstance(x, str)"""
         self.check(b, a)
+
+    def test_ensure_past_builtins_basestring_unchanged(self):
+        u = "from past.builtins import basestring"
+        self.unchanged(u)
+
+    def test_does_not_change_basestring_in_string(self):
+        u = 'print("hello basestring")'
+        self.unchanged(u)
+
+    def test_does_not_change_basestring_in_methodname(self):
+        u = "return encode_basestring('foo')"
+        self.unchanged(u)
