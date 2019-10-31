bba7c700411ea965529a2be2e7328c74eca84e14
Ryan Wersal <ryan@ryanwersal.com>
2019-10-31 16:57:14 -0500

    Remove dctsetcomp fixer in 3to2 as it is supported in 2.7 (fixes #35)
    Adds py2verify tests for dict and set literals

--- py2verify/test_comprehensions.py
+++ py2verify/test_comprehensions.py
@@ -5,3 +5,13 @@ def test_list_comprehension_okay_with_no_parens():
 def test_list_comprehension_okay_with_parens():
     """Confirms that list comprehensions with parens are okay as well. Allows skipping 3to2 stripping such parens."""
     assert [x for x in (1, 2)] == [1, 2]
+
+def test_dict_comprehension_literals():
+    """Confirms that dict comprehension literals are supported."""
+    expected = {"foo": "bar", 17: 12}
+    assert {k: v for k, v in expected.iteritems()} == expected
+
+def test_set_comprehension_literals():
+    """Confirms that set comprehension literals are supported."""
+    initial = [1, 4, 3, 4, 2]
+    assert {v for v in initial} == set([1, 2, 3, 4])
