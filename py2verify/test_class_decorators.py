0e57b09b5c78e4c3425ddfc6a626752d202a2e44
Ryan Wersal <ryan@ryanwersal.com>
2019-10-31 16:53:52 -0500

    Remove 3to2 classdecorator fixer since 2.7 supports it (fixes #34)
    Updates py2verify tests to confirm class decorator behavior

--- py2verify/test_class_decorators.py
+++ py2verify/test_class_decorators.py
@@ -1,9 +1,11 @@
-def foo(cls):
+def class_decorator(cls):
+    cls.added_by_decorator = True
     return cls
 
-@foo
+@class_decorator
 class A(object):
     pass
 
 def test_class_decorator_does_not_explode():
-    x = A()
+    a_instance = A()
+    assert a_instance.added_by_decorator # pylint: disable=no-member
