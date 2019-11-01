bdce86a4e2bfc49921aa08e6a963250868a6c17c
Dalton Dirkson <sodakdoubled@gmail.com>
2019-11-01 11:52:38 -0500

    get rid of extraneous messages in test

--- fixer_suites/two_to_three/tests/test_argspec.py
+++ fixer_suites/two_to_three/tests/test_argspec.py
@@ -9,10 +9,10 @@ def fixer_fixture(two_to_three_test_case):
 def test_import(fixer):
     b = "from inspect import blah, blah, getargspec, blah, blah"
     a = "from inspect import blah, blah, getfullargspec, blah, blah"
-    fixer.check(b, a, "some of the values returned by getargspec are not valid in Python 2 and have no equivalent.")
+    fixer.check(b, a)
 
 
 def test_usage(fixer):
     b = "argspec = inspect.getargspec(func)"
     a = "argspec = inspect.getfullargspec(func)"
-    fixer.check(b, a, "some of the values returned by getargspec are not valid in Python 2 and have no equivalent.")
+    fixer.check(b, a)
